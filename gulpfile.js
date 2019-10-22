bf0870b905fabe106c74b5f09b26ccd726ce9809
Andrey Agafonov <agafonovim@AndyBook.local>
2019-10-22 10:00:53 +0300

    first screen done

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+
+gulp.task('styles', () => {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'assets/css/main.css',
+    ]);
+});
+
+gulp.task('styles', () => {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('scss/**/*.scss', (done) => {
+        gulp.series(['clean', 'styles'])(done);
+    });
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
